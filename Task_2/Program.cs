4bac78f796211ca3bbd9c3a379b5d3ab92d1e998
Sanzhar <dos.0227@yandex.ru>
2022-12-01 15:58:33 +0600

    Task2 задача 11

---
+++ Task_2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача №11
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерировано случайное число - {num}");
+int x1 = num / 100;
+int x2 = num % 10;
+Console.Write($"Без второй цифры это {x1}{x2}");
