fe4b4f856b42fe3a99b3141a7b888411c8fb62c8
Sanzhar <dos.0227@yandex.ru>
2022-12-01 15:58:54 +0600

    Task 3 задача 12

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №12
+
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+int num1 = new Random().Next(10, 100);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine($"Сгенерировано первое число {num1} и второе число {num2}");
+int x = num1 / num2;
+if (num1 == x * num2) // (num1 % num2 == 0)
+{
+    Console.WriteLine($"Второе число кратно первому - в {x} раз"); // Console.WriteLine($"Второе число кратно первому в {num1 / num2} раз");
+}
+else
+{
+    int y = num1 % num2;
+    Console.WriteLine($"Второе число не кратно первому, остаток первого числа от второго равен {y}");
+}
