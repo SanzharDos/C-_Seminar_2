c181506915a6db5445e3735e3b9a2386b0b92f53
Sanzhar <dos.0227@yandex.ru>
2022-12-01 16:27:53 +0600

    Task4 задача

---
+++ Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача - напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+int num = new Random().Next();
+Console.WriteLine($"Сгенерировано случайное число - {num}");
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine($"Число {num} одновременно кратно 7 и 23");
+}
+else if (num % 7 == 0 && num % 23 != 0)
+{
+    Console.WriteLine($"Число {num} кратно 7");
+}
+else if (num % 7 != 0 && num % 23 == 0)
+{
+    Console.WriteLine($"Число {num} кратно 23");
+}
+else
+{
+    Console.WriteLine($"Число {num} не кратно 7 и 23");
+}
